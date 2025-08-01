39e97eba3e874b5f1fcb06d6e57a9033e4cfa924
MilkyWay303 <pomidorowy999@gmail.com>
2025-08-01 19:45:30 +0200

    glass + imgs

--- fizjomed_vue/src/components/views/Oferta.vue
+++ fizjomed_vue/src/components/views/Oferta.vue
@@ -1,28 +1,50 @@
 <template>
-    Magnetoterapia – leczenie polem magnetycznym
-    Laseroterapia
-    Krioterapia – leczenie zimnem
-    Światłoterapia – lampa solux
-    Elektrolecznictwo:
-    prądy interferencyjne
-    prądy diadynamiczne
-    jonoforeza
-    galwanizacja
-    elektrostymulacja
-    prądy TENS
-    prądy KOTZA ,TREBERTA
-    Ultradźwięki
-    Terapia manulana wg. dr Cyriaxa
-    Terapia manualna wg. prof.K.Lewita
-    Kinesiology Taping
-    Masaż leczniczy
-    masaż klasyczny całościowy
-    masaz klasyczny częściowy
-    masaż relaksacyjny
-    elementy terapii punków spustowych
-    Termożele
-    Ćwiczenia usprawniające w poszczegulnych jednostkach chorobowych
-    Ćwiczenia w odciążeniu
-    Ćwiczenia czynne wolne i czynne z oporem
-    Ćwiczenia indywidualne z pacjentem
+    <div class="oferta">
+        <div class="tekst">
+            Magnetoterapia - leczenie polem magnetycznym
+            Laseroterapia
+            Krioterapia - leczenie zimnem
+            Światłoterapia - lampa solux
+            Elektrolecznictwo:
+            prądy interferencyjne
+            prądy diadynamiczne
+            jonoforeza
+            galwanizacja
+            elektrostymulacja
+            prądy TENS
+            prądy KOTZA ,TREBERTA
+            Ultradźwięki
+            Terapia manulana wg. dr Cyriaxa
+            Terapia manualna wg. prof.K.Lewita
+            Kinesiology Taping
+            Masaż leczniczy
+            masaż klasyczny całościowy
+            masaz klasyczny częściowy
+            masaż relaksacyjny
+            elementy terapii punków spustowych
+            Termożele
+            Ćwiczenia usprawniające w poszczegulnych jednostkach chorobowych
+            Ćwiczenia w odciążeniu
+            Ćwiczenia czynne wolne i czynne z oporem
+            Ćwiczenia indywidualne z pacjentem
+        </div>
+    </div>
 </template>
+
+<style scoped>
+
+    .tekst {
+        color: black;
+        font-size: 25px;
+    }
+
+    .oferta {
+        background: rgba(145, 125, 125, 0.1);
+        border-radius: 16px;
+        box-shadow: 0 4px 30px rgba(0, 0, 0, 0.5);
+        backdrop-filter: blur(2px);
+        -webkit-backdrop-filter: blur(2px);
+        border: 1px solid rgba(235, 168, 168, 0.4);
+    }
+</style>
+
